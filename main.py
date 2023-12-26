7b8b2ab6a9a081c1d1ddb711e302742d2c0e5477
Sidhant Roymoulik <sidhant.roymoulik@gmail.com>
2023-12-26 16:00:33 -0500

    intial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from programs import *
+
+# from numpy import random
+
+score_matrix = [[3, 0], [5, 1]]
+
+programs = [
+    AllC(),
+    TFT(),
+    TF2T(),
+    T2FT(),
+    SlowTFT(),
+    SoftMajority(),
+    Pavlov(),
+    AllD(),
+    Mistrust(),
+    HardMajority(),
+    Prober(),
+    # Random(),
+]
+
+copies = 5
+rounds = 150
+num_programs = len(programs)
+
+for i in range(num_programs * copies):
+    for j in range(i + 1, num_programs * copies):
+        p1 = programs[i // copies]
+        p1_history = ""
+
+        p2 = programs[j // copies]
+        p2_history = ""
+
+        for k in range(rounds):
+            p1_state = ";".join([str(k), p1_history, p2_history])
+            p1_response = p1.reponse(p1_state)
+
+            p2_state = ";".join([str(k), p2_history, p1_history])
+            p2_response = p2.reponse(p2_state)
+
+            p1.score += score_matrix[ord(p1_response) - ord("C")][
+                ord(p2_response) - ord("C")
+            ]
+            p2.score += score_matrix[ord(p2_response) - ord("C")][
+                ord(p1_response) - ord("C")
+            ]
+
+            p1_history += p1_response
+            p2_history += p2_response
+
+for i in range(num_programs):
+    # Sort programs by score
+    for j in range(i + 1, num_programs):
+        if programs[j].score > programs[i].score:
+            programs[i], programs[j] = programs[j], programs[i]
+
+    print("-" * 50)
+    print(" " + programs[i].name.ljust(25, " ") + "| " + str(programs[i].score))
+
+print("-" * 50)
---
+++ programs.py
@@ -0,0 +1,164 @@
+from numpy import random
+
+
+# Nice Programs
+class AllC:
+    name = "All C"
+    score = 0
+
+    def reponse(self, state):
+        return "C"
+
+
+class TFT:
+    name = "Tit For Tat"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if tick == 0:
+            return "C"
+        else:
+            return their_history[-1]
+
+
+class TF2T:
+    name = "Tit For 2 Tats"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if tick <= 1:
+            return "C"
+        else:
+            if their_history[-1] == "D" and their_history[-2] == "D":
+                return "D"
+            else:
+                return "C"
+
+
+class T2FT:
+    name = "2 Tit For Tat"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if tick <= 1:
+            return "C"
+        else:
+            if their_history[-1] == "D" or their_history[-2] == "D":
+                return "D"
+            else:
+                return "C"
+
+
+class SlowTFT:
+    name = "Slow Tit For Tat"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if tick <= 1:
+            return "C"
+        else:
+            if their_history[-1] == "D" and their_history[-2] == "D":
+                return "D"
+            if their_history[-1] == "C" and their_history[-2] == "C":
+                return "C"
+            else:
+                return my_history[-1]
+
+
+class SoftMajority:
+    name = "Soft Majority"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if my_history.count("C") >= my_history.count("D"):
+            return "C"
+        else:
+            return "D"
+
+
+class Pavlov:
+    name = "Pavlov"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if tick == 0:
+            return "C"
+        else:
+            if their_history[-1] == my_history[-1]:
+                return "C"
+            else:
+                return "D"
+
+
+# Nasty
+class AllD:
+    name = "All D"
+    score = 0
+
+    def reponse(self, state):
+        return "D"
+
+
+class Mistrust:
+    name = "Mistrust"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if tick == 0:
+            return "D"
+        else:
+            return their_history[-1]
+
+
+class HardMajority:
+    name = "Hard Majority"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if my_history.count("C") > my_history.count("D"):
+            return "C"
+        else:
+            return "D"
+
+
+class Prober:
+    name = "Prober"
+    score = 0
+
+    def reponse(self, state):
+        tick, my_history, their_history = state.split(";")
+        tick = int(tick)
+        if tick == 0:
+            return "C"
+        elif tick <= 2:
+            return "D"
+        else:
+            if their_history[1] == "C" and their_history[2] == "C":
+                return "D"
+            else:
+                return their_history[-1]
+
+
+# Neither
+class Random:
+    name = "Random"
+    score = 0
+
+    def reponse(self, state):
+        return random.choice(["C", "D"])
