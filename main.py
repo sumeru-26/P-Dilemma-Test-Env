eb5ff50d65eca715c8001a7ba0b9e17a3f97565c
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2023-12-28 13:41:59 -0800

    fixed duplicate bots in final table, added submission file

--- main.py
+++ main.py
@@ -32,12 +32,30 @@ print(" " + "Strategy Name".ljust(25, " ") + "| Avg Score Per Round")
 print("-" * 50)
 
 summary_data = results.summarise()
+
+data_dict = {}
+for i in summary_data:
+    if i.Name in data_dict:
+        data_dict.update({i.Name : (data_dict[i.Name]+i.Median_score)/2})
+    else:
+        data_dict.update({i.Name : i.Median_score})
+
+'''
 for i in range(0, len(players)):
     print(
         " "
         + summary_data[i].Name.ljust(25, " ")
         + "| "
         + str(round(summary_data[i].Median_score, 3))
+    )
+'''
+
+for name,score in data_dict.items():
+    print(
+        " "
+        + name.ljust(25, " ")
+        + "| "
+        + str(round(score, 3))
     )
 
 print("-" * 50)
